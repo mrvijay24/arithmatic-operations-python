89c397b6e8d05d30ea8a8bd544ada076aa54d778
mrvijay24 <110984700+mrvijay24@users.noreply.github.com>
2022-11-24 19:54:45 +0530

    Add files via upload

---
+++ arithmatic operations.py
@@ -0,0 +1,18 @@
+#arithmatic operations
+print('enter the values')
+a=int(input())
+b=int(input())
+c=a+b
+print('addition:',c)
+d=a-b
+print('substraction',d)
+e=a*b
+print('multiplication',e)
+f=a/b
+print('division',f)
+g=a%b
+print('modul division',g)
+h=a**b
+print('exponentiation',h)
+j=a//b
+print('floor division',j)
